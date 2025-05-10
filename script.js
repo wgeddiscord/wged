1bb3780341be38a966c96a247a1aa467de6271fe
wged <zyneticartz@gmail.com>
2025-05-10 17:56:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const music = document.getElementById("bg-music");
+const toggleBtn = document.getElementById("sound-toggle");
+
+let isMuted = false;
+
+toggleBtn.addEventListener("click", () => {
+  isMuted = !isMuted;
+
+  toggleBtn.style.opacity = 0;
+
+  setTimeout(() => {
+    if (isMuted) {
+      music.pause();
+      toggleBtn.src = "images/mute.png";
+    } else {
+      music.play();
+      toggleBtn.src = "images/sound.png";
+    }
+    toggleBtn.style.opacity = 1;
+  }, 200);
+});
 
