f8a184742fc1cc0931bc60ad3e62c8b302985ab2
wged <zyneticartz@gmail.com>
2025-05-10 18:00:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const music = document.getElementById("bg-music");
 const toggleBtn = document.getElementById("sound-toggle");
 
-let isMuted = false;
+let isMuted = true;
 
 toggleBtn.addEventListener("click", () => {
   isMuted = !isMuted;
@@ -20,3 +20,19 @@ toggleBtn.addEventListener("click", () => {
   }, 200);
 });
 
+// Lancer la musique au 1er clic n’importe où
+document.body.addEventListener("click", () => {
+  if (isMuted) return;
+  music.play();
+}, { once: true });
+
+// Générer des particules
+const container = document.getElementById("particles-container");
+for (let i = 0; i < 20; i++) {
+  const p = document.createElement("div");
+  p.className = "particle";
+  p.style.left = `${Math.random() * 100}%`;
+  p.style.top = `${Math.random() * 100}%`;
+  p.style.animationDelay = `${Math.random() * 5}s`;
+  container.appendChild(p);
+}
